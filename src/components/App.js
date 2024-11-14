0feef22216c3dc369cbff1d6dba66a91d5f6a904
Al Cerdan Lico <alcerdan@yahoo.com>
2024-11-13 21:59:29 -0800

    complete class setstate lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,56 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import { render } from "@testing-library/react";
 
-function App() {
-  const [items, setItems] = useState(itemData);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+// function App() {
+//   const [items, setItems] = useState(itemData);
+//   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+//   function handleDarkModeClick() {
+//     setIsDarkMode((isDarkMode) => !isDarkMode);
+//   }
+
+//   return (
+//     <div className={"App " + (isDarkMode ? "dark" : "light")}>
+//       <header>
+//         <h2>Shopster</h2>
+//         <button onClick={handleDarkModeClick}>
+//           {isDarkMode ? "Dark" : "Light"} Mode
+//         </button>
+//       </header>
+//       <ShoppingList items={items} />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+
+  state = {
+    items: itemData,
+    isDarkMode: false
+  }
+  
+  handleDarkModeClick = () => {
+    this.setState((prevState) => ({
+      isDarkMode: !prevState.isDarkMode
+    }))
   }
 
-  return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={items} />
-    </div>
-  );
+  render() {
+    const { items, isDarkMode } = this.state
+    return (
+      <div className={"App " + (isDarkMode ? "dark" : "light")}>
+        <header>
+          <h2>Shopster</h2>
+          <button onClick={this.handleDarkModeClick}>
+            {isDarkMode ? "Dark" : "Light"} Mode
+          </button>
+        </header>
+        <ShoppingList items={items} />
+      </div>
+    )
+  }
 }
 
 export default App;
